2b706dc455a10701fb42931e3e47836f2dd5ad75
guilherme-cmd <54598542+guilherme-cmd@users.noreply.github.com>
2023-09-11 21:42:26 -0300

    fix

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-require('dotenv').config()
-
-
-const express = require ('express')
+const express = require('express');
 const cors = require('cors');
-const path = require ('path')
-const app = express ()
+const path = require('path');
+const app = express();
 
-const apiRouter = require('./api/routes/apiRouter')
-app.use ('/api', apiRouter)
+const apiRouter = require('./api/routes/apiRouter');
+app.use('/api', apiRouter);
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/app', express.static (path.join (__dirname, '/public')))
-let port = process.env.PORT || 3000
-app.listen (port)
+
+// Servir arquivos estáticos da pasta "public" para o root e outras rotas não correspondidas
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Redirecionar todas as solicitações não correspondidas para a página inicial
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
